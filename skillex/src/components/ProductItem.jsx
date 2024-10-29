aac3ce8680a29f3c99a8c272b0bdbe6631a01bab
grigoryan0326 <grigoryan0326@mail.ru>
2024-10-30 00:52:51 +0400

    skillex react-filter first commit

---
+++ skillex/src/components/ProductItem.jsx
@@ -0,0 +1,34 @@
+import { FaStar } from "react-icons/fa6"
+
+const ProductItem = ({ products }) => {
+  return (
+    <ul className='products__list'>
+      {products.length ? (
+        products.map((product) => (
+          <li
+            className='product__item'
+            key={product.id}
+          >
+            <img
+              src={product.imageUrl}
+              alt={product.name}
+              className='product__img'
+            />
+            <div className='product__content'>
+              <h3 className='product__name'>{product.name}</h3>
+              <p className='product__price'>{product.price} $</p>
+              <p className='product__rating'>
+                <FaStar className='product__star' />
+                {product.rating}
+              </p>
+              <p className='product__btn'>Add to Cart</p>
+            </div>
+          </li>
+        ))
+      ) : (
+        <p>Nothing to show</p>
+      )}
+    </ul>
+  )
+}
+export default ProductItem
---
+++ skillex/src/containers/App/App.js
@@ -0,0 +1,14 @@
+import Header from "../../components/Header"
+import ProductsList from "../../components/ProductsList"
+import "./App.scss"
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <ProductsList />
+    </div>
+  )
+}
+
+export default App
